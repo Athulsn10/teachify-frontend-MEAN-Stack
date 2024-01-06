f5a174e79b1573c1946bf799597d2724fa532f0b
Athulsn10 <athulnair10@outlook.com>
2024-01-07 00:54:36 +0530

    url

--- src/app/userservices/user-service.service.ts
+++ src/app/userservices/user-service.service.ts
@@ -9,7 +9,7 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class UserService {
   // SERVER_URL = "http://localhost:3000/api"
-  SERVER_URL = "https://teachify-3fxy.onrender.com"
+  SERVER_URL = "https://teachify-3fxy.onrender.com/api"
 
   constructor(private http: HttpClient) { }
   registerUser(userDetails: any){
