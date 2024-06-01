840d973ecde86a65376edce9ffbb7c69555b61c1
Athulsn10 <athulnair10@outlook.com>
2024-06-01 17:03:36 +0530

    added loading icon logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../userservices/user-service.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +9,40 @@ import { UserService } from '../userservices/user-service.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router ){}
+  isLoading = false;
+  showToast = false;
+  toastMessage = ''; 
 
-loginUser(val:any){
-  // console.log(val);
+  constructor(private userService: UserService, private router: Router) {}
 
-  this.userService.loginUser(val).subscribe(
-    (response) => {
-      console.log('User registration successful');
-      localStorage.setItem('currentUser', JSON.stringify(response));
-      this.router.navigate(['/user/dashboard']);
-    },
-    (error) => {
-      console.error('Error during user registration:', error);
+  loginUser(val: any) {
+    this.isLoading = true;
+    const { email, password } = val
+    if (!email || !password) {
+      this.showToast = true;
+      this.toastMessage = 'Please fill all the fields';
+      this.isLoading = false;
+      return
     }
-  );
-  
-}
+    this.userService.loginUser(val).subscribe(
+      (response) => {
+        // console.log('User login successful');
+        localStorage.setItem('currentUser', JSON.stringify(response));
+        this.router.navigate(['/user/dashboard']);
+        this.isLoading = false;
+      },
+      (error: HttpErrorResponse) => {
+        this.isLoading = false;
+        if (error.status === 401) {
+          this.toastMessage = error.error.message;
+          this.showToast = true;
+          setTimeout(() => {
+            this.showToast = false;
+          }, 3000);
+        } else {
+          // console.error('Error during user login:', error);
+        }
+      }
+    );
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../userservices/user-service.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -8,17 +9,40 @@ import { UserService } from '../userservices/user-service.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(private userService: UserService, private router: Router ){}
-  registerUser(val: any){
-    // console.log(val);
+  constructor(private userService: UserService, private router: Router) { }
+
+  isLoading = false;
+  showToast = false;
+  toastMessage = '';
+
+  registerUser(val: any) {
+    this.isLoading = true;
+    const { name, email, password } = val
+    if (!name || !email || !password) {
+      this.showToast = true;
+      this.toastMessage = 'Please fill all the fields';
+      this.isLoading = false;
+      return
+    }
     this.userService.registerUser(val).subscribe(
       (response) => {
-        console.log('User registration successful');
+        // console.log('User registration successful');
         localStorage.setItem('currentUser', JSON.stringify(response));
         this.router.navigate(['/user/dashboard']);
+        this.isLoading = false;
       },
-      (error) => {
-        console.error('Error during user registration:', error);
+      (error: HttpErrorResponse) => {
+        this.isLoading = false;
+        if (error.status === 400) {
+          // console.log(error);
+          this.toastMessage = error.error.message;
+          this.showToast = true;
+          setTimeout(() => {
+            this.showToast = false;
+          }, 3000);
+        } else {
+          // console.error('Error during user login:', error);
+        }
       }
     );
   }
